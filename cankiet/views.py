fa330a49d6ef93dc10ce4195e8120297689a041b
Nilesh Agrahari <nagrahari539@gmail.com>
2024-11-20 16:48:40 +0530

    login page added

--- cankiet/views.py
+++ cankiet/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index2(request):
+    return render(request, 'cankiet/index2.html')
+
+def login(request):
+    return render(request, 'cankiet/login.html')    
+ 
