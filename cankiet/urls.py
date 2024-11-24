9785ef41e857e6568232e4b7160d68243a94c6ce
Nilesh Agrahari <nagrahari539@gmail.com>
2024-11-24 23:24:41 +0530

    session created

--- cankiet/urls.py
+++ cankiet/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('index2/',views.index2, name='index2'),
     path('orders/',views.orders, name='orders'),
     path('check/', views.login_check, name='check'),
+    path('logout/', views.logout, name='logout'),
 
 ]
--- cankiet/views.py
+++ cankiet/views.py
@@ -5,11 +5,20 @@ from .forms import LoginForm
 from .models import User
 def index2(request):
     user=User.objects.all()
+    # Check if 'user_id' exists in the session
+    if 'user' not in request.session:
+        return redirect('login')  # Replace 'login' with the name of your login URL
+    # If session exists, render the index page
     return render(request, 'cankiet/index2.html', {'user': user})
 
 def login(request):
     user=User.objects.all()
-    return render(request, 'cankiet/login.html', {'user': user})    
+    return render(request, 'cankiet/login.html', {'user': user})  
+
+def logout(request):
+    # Clear the session
+    request.session.flush()  # Removes all session data
+    return redirect('login')  # Redirect to the login page  
  
 def orders(request):
     user=User.objects.all()
@@ -26,7 +35,13 @@ def login_check(request):
                 user = User.objects.get(u_id=u_id)
                 # Check if the entered password matches the hashed password
                 if check_password(password, user.password):
-                    request.session['user_id'] = user.u_id
+                    # Store all user attributes in the session
+                    request.session['user'] = {
+                    'user_id': user.u_id,
+                    'name': user.name,
+                    'phone': user.phone,
+                    'dept': user.branch,
+                    }
                     return redirect('index2')
                 else:
                     form.add_error(None, "Invalid User ID or Password")
