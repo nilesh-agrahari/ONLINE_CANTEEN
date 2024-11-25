e748a3668180c623597c02d59dff49b040451af3
Nilesh Agrahari <nagrahari539@gmail.com>
2024-11-25 21:48:16 +0530

    s

--- cankiet/models.py
+++ cankiet/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password
-from django.utils import timezone
+from django.utils.timezone import now
 # Create your models here.
 
 class User(models.Model):
@@ -13,7 +13,7 @@ class User(models.Model):
         ('Mba','MBA'),
     ]
 
-    u_id=models.CharField(max_length=12,)
+    u_id=models.CharField(max_length=12,primary_key=True)
     name=models.CharField(max_length=30)
     phone=models.CharField(max_length=10)
     password=models.CharField(max_length=20)
@@ -29,7 +29,7 @@ class User(models.Model):
         return self.u_id
 
 class Items(models.Model):
-    i_no=models.CharField(max_length=12)
+    i_no=models.CharField(max_length=12,primary_key=True)
     item=models.CharField(max_length=20)
     i_image=models.ImageField(upload_to='images/')
     price=models.IntegerField()
@@ -40,3 +40,19 @@ class Items(models.Model):
     def __str__(self):
         return self.item
 
+class Canteen(models.Model):
+    c_no=models.CharField(max_length=2,primary_key=True,)
+    c_name=models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.c_name
+    
+class Order(models.Model):
+    o_no = models.AutoField(primary_key=True)  # Auto-incrementing primary key
+    o_date = models.DateTimeField(default=now)  # Automatically stores the current date and time
+    i_no = models.ForeignKey('Item', on_delete=models.CASCADE)  # Foreign key to Item table
+    u_id = models.ForeignKey('User', on_delete=models.CASCADE)  # Foreign key to User table
+    c_no = models.ForeignKey('Canteen', on_delete=models.CASCADE)  # Foreign key to Canteen table
+
+    def __str__(self):
+        return f"Order {self.o_no} - {self.o_date.strftime('%Y-%m-%d %H:%M:%S')}"
